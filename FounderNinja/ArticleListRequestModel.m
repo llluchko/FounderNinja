f2761cee36026906f3a25acd5c025ec4140c1760
llluchko <l.mladenov21@gmail.com>
2016-10-28 18:29:32 +0300

    Implement JSON Trasnformers to working with the JSON data

--- FounderNinja/ArticleListRequestModel.m
+++ FounderNinja/ArticleListRequestModel.m
@@ -11,4 +11,40 @@
 
 @implementation ArticleListRequestModel
 
++ (NSDateFormatter *)dateFormatter {
+	NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+	dateFormatter.dateFormat = @"yyyyMMdd";
+	return dateFormatter;
+}
+
+#pragma mark - Mantle JSONKeyPathsByPropertyKey
+
++ (NSDictionary *)JSONKeyPathsByPropertyKey {
+	return @{
+			 @"query" : @"q",
+			 @"articlesFromDate" : @"begin_date",
+			 @"articlesToDate" : @"end_date"
+			 };
+}
+
+#pragma mark - JSON Transformers
+
++ (NSValueTransformer *)articlesToDateJSONTransformer {
+	return [MTLValueTransformer transformerUsingForwardBlock:^id(NSString *dateString, BOOL *success, NSError *__autoreleasing *error) {
+		return [self.dateFormatter dateFromString:dateString];
+	} reverseBlock:^id(NSDate *date, BOOL *success, NSError *__autoreleasing *error) {
+		return [self.dateFormatter stringFromDate:date];
+	}];
+
+}
+
++ (NSValueTransformer *)articlesFromDateJSONTransformer {
+	return [MTLValueTransformer transformerUsingForwardBlock:^id(NSString *dateString, BOOL *success,
+																 NSError *__autoreleasing *error) {
+		return [self.dateFormatter dateFromString:dateString];
+	} reverseBlock:^id(NSDate *date, BOOL *success, NSError *__autoreleasing *error) {
+		return [self.dateFormatter stringFromDate:date];
+	}];
+}
+
 @end
