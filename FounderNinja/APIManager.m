ae74da8e157b6fe295f6c9b68f51e5077dee0603
llluchko <l.mladenov21@gmail.com>
2016-11-02 17:47:36 +0200

    Start implementation of API Manager

--- FounderNinja/APIManager.m
+++ FounderNinja/APIManager.m
@@ -8,7 +8,17 @@
 //
 
 #import "APIManager.h"
+#import "Mantle.h"
+
+static NSString *const kArticlesListPath = @"/svc/search/v2/articlesearch.json";
+static NSString *const kApiKey = @"efa48cdfbd124ac8a934619d1814ac2a";
 
 @implementation APIManager
+
+- (NSURLSessionDataTask *)getArticlesWithRequestModel:(ArticleListRequestModel *)requestModel success:(void (^)(ArticleListResponseModel *responseModel))success failure:(void (^)(NSError *error))failure{
+	
+	NSDictionary *parameters = [MTLJSONAdapter JSONDictionaryFromModel:requestModel error:nil];
+
+}
 
 @end
